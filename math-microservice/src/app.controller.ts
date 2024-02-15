4d89e679bb75555dcb4a62a1f38a267abae3b125
MuhammadAfshal1 <m.afshal@redbuffer.net>
2024-02-15 15:13:43 +0500

    commited.

--- math-microservice/src/app.controller.ts
+++ math-microservice/src/app.controller.ts
@@ -1,16 +1,22 @@
 import { Controller, Get, Logger } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
-  const logger = new Logger('AppController')
+const logger = new Logger('AppController');
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/')
+  get() {
+    return 'get';
+  }
 
   @MessagePattern('sum')
   async sumNumbers(data: Array<number>) {
-    logger.log('math microservice recieved a request to sum ' +  data.toString())
-    return { result: data.reduce((a, b) => a + b) }
+    logger.log(
+      'math microservice recieved a request to sum ' + data.toString(),
+    );
+    return { result: data.reduce((a, b) => a + b) };
   }
-  
 }
--- math-microservice/src/main.ts
+++ math-microservice/src/main.ts
@@ -3,23 +3,25 @@ import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
-const logger = new Logger('Main')
+const logger = new Logger('Main');
 
 const microServiceOptions = {
   transport: Transport.TCP,
   options: {
     host: '127.0.0.1',
-    port : 8123
-  }
-}
+    port: 8123,
+  },
+};
 
 async function bootstrap() {
-
-  const app = await NestFactory.createMicroservice(AppModule, microServiceOptions)
-
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    microServiceOptions,
+  );
+  const app2 = await NestFactory.create(AppModule);
   app.listen(() => {
-    logger.log('Math microservice up and running Boii')
-  })
-  
+    logger.log('Math microservice up and running Boii');
+  });
+  app2.listen(4001);
 }
 bootstrap();
